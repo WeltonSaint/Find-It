fc5067b8b09adb78c232bc3f966d1e4d2048c1e2
WeltonSaint <WeltonSaint@WS>
2016-10-02 21:36:10 -0300

    Versão do protótipo de telas

---
+++ app/src/main/java/lps/com/br/find_it/InsertActivity.java
@@ -0,0 +1,31 @@
+package lps.com.br.find_it;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class InsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Spinner spinCategory = (Spinner) findViewById(R.id.spin_category);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinCategory.setAdapter(adapter);
+
+    }
+
+}
